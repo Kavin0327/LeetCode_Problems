4b09ee9440fe36a5fda1a5c1fc85f669414d770e
Kavin K <121722829+Kavin0327@users.noreply.github.com>
2024-02-29 22:09:37 +0530

    Time: 12 ms (58.59%), Space: 66.1 MB (45.70%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.java
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -6,15 +6,16 @@ class Solution {
         while(!queue.isEmpty()){
             int size = queue.size();
             int prev = 0;
-            if(level%2 == 0)
-                prev = Integer.MIN_VALUE;
-            else
+            if((level&1) == 1)
                 prev = Integer.MAX_VALUE;
+            else
+                prev = Integer.MIN_VALUE;
+                                
             for(int i=0;i<size;i++){
                 TreeNode curr = queue.poll();
-                if(level%2 == 0 && (prev >= curr.val || curr.val%2 == 0))
+                if((level&1) == 0 && (prev >= curr.val || (curr.val&1) == 0))
                     return false;
-                if(level%2 == 1 && (prev <=  curr.val|| curr.val%2 == 1))
+                if((level&1) == 1 && (prev <=  curr.val|| (curr.val&1) == 1))
                     return false;
                 prev = curr.val;
                 if(curr.left != null)
