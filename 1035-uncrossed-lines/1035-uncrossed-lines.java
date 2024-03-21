b168f633241c2b4d8fc3ff7b9b124a86a1167777
Kavin K <121722829+Kavin0327@users.noreply.github.com>
2024-03-21 12:33:37 +0530

    Time: 6 ms (73.47%), Space: 44.2 MB (23.97%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -1,24 +1,19 @@
 class Solution {
-    //Memoization Apporach
+    //DP(Tabulation method)Apporach
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
-        Integer memo[][] = new Integer[n][m];
-        return helper(nums1,nums2,0,0,n,m,memo);
-    }
-    public int helper(int arr1[],int arr2[],int index1,int index2,int n,int m,Integer memo[][]){
-        if(index1 == n || index2 == m)
-            return 0;
-        if(memo[index1][index2] != null){
-            return memo[index1][index2];
+        int dp[][] = new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(nums1[i-1] == nums2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
         }
-        if(arr1[index1] == arr2[index2]){
-            return memo[index1][index2] = 1 + helper(arr1,arr2,index1+1,index2+1,n,m,memo);
-        }
-        else{
-            int first = helper(arr1,arr2,index1+1,index2,n,m,memo);
-            int second = helper(arr1,arr2,index1,index2+1,n,m,memo);
-            return memo[index1][index2] = Math.max(first,second);
-        }
+        return dp[n][m];
     }
 }
