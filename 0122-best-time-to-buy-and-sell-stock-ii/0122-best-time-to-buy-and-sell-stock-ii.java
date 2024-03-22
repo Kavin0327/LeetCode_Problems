7337a4d8a290f03ce4cb5960059db40b8e8e0f18
Kavin K <121722829+Kavin0327@users.noreply.github.com>
2024-03-22 10:30:31 +0530

    Time: 3 ms (10.24%), Space: 49.6 MB (5.03%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    //Memoization Approach
+    public int maxProfit(int[] prices) {
+        Integer memo[][] = new Integer[prices.length][2];
+        return helper(prices,0,0,memo);
+    }
+    // posses -> how many stock hold 
+    public int helper(int arr[],int index,int posses,Integer memo[][]){
+        if(index == arr.length)
+            return 0;
+        if(memo[index][posses] != null) 
+            return memo[index][posses];
+        if(posses == 0){
+            int buy = helper(arr,index+1,1,memo)-arr[index];
+            int skipBuy = helper(arr,index+1,0,memo);
+            return memo[index][posses] = Math.max(buy,skipBuy);
+        }
+        else{
+            int sell = helper(arr,index+1,0,memo)+arr[index];
+            int skipSell = helper(arr,index+1,1,memo);
+            return memo[index][posses] = Math.max(sell,skipSell);
+        }
+    }
+}
