09462a254a7d2e4bb353a45af6ea6b8ac1d72c09
Kavin K <121722829+Kavin0327@users.noreply.github.com>
2024-03-23 10:51:31 +0530

    Time: 328 ms (5.02%), Space: 156.8 MB (34.26%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        // Creating Adjacency Matrix
+        if(source == destination)
+            return true;
+        Map<Integer,List<Integer>> adjacencyMatrix = new HashMap<>();
+        for(int arr[]:edges){
+            if(!adjacencyMatrix.containsKey(arr[0]))
+                adjacencyMatrix.put(arr[0],new ArrayList<>());
+            if(!adjacencyMatrix.containsKey(arr[1]))
+                adjacencyMatrix.put(arr[1],new ArrayList<>());
+            adjacencyMatrix.get(arr[0]).add(arr[1]);
+            adjacencyMatrix.get(arr[1]).add(arr[0]);
+        }
+        // System.out.println(adjacencyMatrix);
+        return DFS(adjacencyMatrix,source,destination,new HashSet<>());
+    }
+    
+    public boolean DFS(Map<Integer,List<Integer>> map,int src,int des,Set<Integer> visited){
+        visited.add(src);
+        List<Integer> list = map.get(src);
+        if(list != null){
+            for(int edge:list){
+                if(!visited.contains(edge)){
+                    DFS(map,edge,des,visited);
+                }
+            }   
+        }
+        return visited.contains(des);
+    }
+}
