5f8fa03472258bbb65805294fe9dbb5c2eda695e
Kavin K <121722829+Kavin0327@users.noreply.github.com>
2024-04-30 18:47:30 +0530

    Time: 1 ms (80.19%), Space: 41.4 MB (47.79%) - LeetHub

---
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean strongPasswordCheckerII(String password) {
+        int n = password.length();
+        if(n < 8){ //check length at most 8
+            return false;
+        }
+        for(int i=1;i<n;i++){ // adjacent same condition
+            if(password.charAt(i-1) == password.charAt(i))
+                return false;
+        }
+        boolean upper = false;
+        boolean lower = false;
+        boolean special = false;
+        boolean number = false;
+        for(int i=0;i<n;i++){
+            char c = password.charAt(i);
+            if(Character.isUpperCase(c)){
+                upper = true;
+            }
+            else if(Character.isLowerCase(c)){
+                lower = true;
+            }
+            else if(Character.isDigit(c)){
+                number = true;
+            }
+            else if(c != ' '){
+                special = true;
+            }
+        }
+        return upper && lower && number && special;
+    }
+}
